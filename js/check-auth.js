213835e9b9701895f44d5b5986d6ac109899c551
dapafosa-tech <dapafosa@gmail.com>
2026-08-08 03:03:36 +0300

    Update check-auth.js

--- js/check-auth.js
+++ js/check-auth.js
@@ -1,19 +1,20 @@
 // ============================================
-// check-auth.js - УНІВЕРСАЛЬНА ПЕРЕВІРКА АВТОРИЗАЦІЇ ТА БАНА
+// check-auth.js - ПЕРЕВІРКА АВТОРИЗАЦІЇ
 // ============================================
 
 (async function checkAuthAndBan() {
     try {
-        // ЯКЩО МИ ВЖЕ НА СТОРІНЦІ BANNED - НІЧОГО НЕ РОБИМО!
-        if (window.location.pathname === '/banned') {
+        var currentPath = window.location.pathname;
+        
+        // ЯКЩО МИ ВЖЕ НА СТОРІНЦІ BANNED - НІЧОГО НЕ РОБИМО
+        if (currentPath === '/banned') {
             return;
         }
         
         var userData = localStorage.getItem('userData');
-        var currentPath = window.location.pathname;
-        var publicPages = ['/login', '/register', '/banned', '/'];
+        var publicPages = ['/login', '/register', '/'];
         
-        // Якщо немає даних користувача
+        // ЯКЩО НЕМАЄ ДАНИХ - НА ЛОГІН
         if (!userData) {
             if (publicPages.indexOf(currentPath) === -1) {
                 window.location.href = '/login';
@@ -23,38 +24,34 @@
 
         var user = JSON.parse(userData);
         
-        // Якщо користувач забанений - редірект на banned
+        // ЯКЩО КОРИСТУВАЧ ЗАБАНЕНИЙ - КИДАЄМО НА BANNED
         if (user.is_banned === true) {
             window.location.href = '/banned';
             return;
         }
 
-        // Перевіряємо статус бана в БД
+        // ПЕРЕВІРЯЄМО БАН В БД
         if (user.id) {
-            try {
-                var response = await fetch(SUPABASE_URL + '/rest/v1/users?id=eq.' + user.id + '&select=is_banned,ban_reason', {
-                    headers: {
-                        'apikey': SUPABASE_ANON_KEY,
-                        'Authorization': 'Bearer ' + SUPABASE_ANON_KEY
-                    }
-                });
-
-                if (response.ok) {
-                    var data = await response.json();
-                    if (data && data.length > 0 && data[0].is_banned === true) {
-                        user.is_banned = true;
-                        user.ban_reason = data[0].ban_reason || 'Порушення правил платформи';
-                        localStorage.setItem('userData', JSON.stringify(user));
-                        window.location.href = '/banned';
-                        return;
-                    }
+            var response = await fetch(SUPABASE_URL + '/rest/v1/users?id=eq.' + user.id + '&select=is_banned,ban_reason', {
+                headers: {
+                    'apikey': SUPABASE_ANON_KEY,
+                    'Authorization': 'Bearer ' + SUPABASE_ANON_KEY
                 }
-            } catch (e) {
-                console.error('Ban check error:', e);
+            });
+
+            if (response.ok) {
+                var data = await response.json();
+                if (data && data.length > 0 && data[0].is_banned === true) {
+                    user.is_banned = true;
+                    user.ban_reason = data[0].ban_reason || 'Порушення правил платформи';
+                    localStorage.setItem('userData', JSON.stringify(user));
+                    window.location.href = '/banned';
+                    return;
+                }
             }
         }
 
-        // Якщо на публічній сторінці і авторизований - редірект на дашборд
+        // ЯКЩО НА ПУБЛІЧНІЙ СТОРІНЦІ - НА ДАШБОРД
         if (publicPages.indexOf(currentPath) !== -1 && user.id) {
             window.location.href = '/dashboard';
         }
